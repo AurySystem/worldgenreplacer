284970b964991180bd965ebaf0dd40f2c7feab81
AurySystem <22009843+AurySystem@users.noreply.github.com>
2025-03-30 15:08:34 -0400

    .gitingore and remove template comment

--- src/main/java/gay/aurum/noisereplacer/NoiseReplacer.java
+++ src/main/java/gay/aurum/noisereplacer/NoiseReplacer.java
@@ -6,9 +6,6 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 public class NoiseReplacer implements ModInitializer {
-	// This logger is used to write text to the console and the log file.
-	// It is considered best practice to use your mod name as the logger's name.
-	// That way, it's clear which mod wrote info, warnings, and errors.
 	public static final Logger LOGGER = LoggerFactory.getLogger("NoiseReplacer");
 
 	@Override
